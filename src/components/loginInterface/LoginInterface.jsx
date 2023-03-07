f1e9a5b1886ecf0e8ef9c5bdf0c565c704dfe903
“Niek” <n.veenstra02@gmail.com>
2023-03-07 16:22:15 +0100

    add login interface

---
+++ src/components/loginInterface/LoginInterface.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledLoginInterface = styled.div``;
+
+const LoginInterface = () => {
+  return (
+    <StyledLoginInterface>
+      <h2>Log in to your account</h2>
+      <form>
+        
+      </form>
+      <button type="submit"></button>
+    </StyledLoginInterface>
+  );
+};
+
+export default LoginInterface;
