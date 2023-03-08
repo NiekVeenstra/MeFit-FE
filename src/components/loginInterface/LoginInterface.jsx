3fd7edfdefdcdc2931fd7db24de97b4a098934a3
“Niek” <n.veenstra02@gmail.com>
2023-03-08 12:56:53 +0100

    add keycloack to login page

--- src/components/loginInterface/LoginInterface.jsx
+++ src/components/loginInterface/LoginInterface.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import keycloak from "../../keycloak";
 
 const StyledLoginInterface = styled.div``;
 
@@ -7,10 +8,21 @@ const LoginInterface = () => {
   return (
     <StyledLoginInterface>
       <h2>Log in to your account</h2>
-      <form>
-        
-      </form>
-      <button type="submit"></button>
+      <div style={{border:"solid red 1px"}}>
+        <h1>Start Page</h1>
+
+        <section>
+          {!keycloak.authenticated && <button onClick={() => keycloak.login()}>Login</button>}
+          {keycloak.authenticated && <button onClick={() => keycloak.logout()}>Logout</button>}
+        </section>
+
+        {keycloak.token && (
+          <div>
+            <h4>Token</h4>
+            <pre>{keycloak.token}</pre>
+          </div>
+        )}
+      </div>
     </StyledLoginInterface>
   );
 };
