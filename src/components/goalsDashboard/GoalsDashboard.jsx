36f66bb6f8ecbbbc843b5dcefa9de45be71fea6a
Meg <megstitu@gmail.com>
2023-03-08 14:49:58 +0100

    edit the GoalsDashboard

--- src/components/goalsDashboard/GoalsDashboard.jsx
+++ src/components/goalsDashboard/GoalsDashboard.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const GoalsDashboard = () => {
-  return (
-    <div>GoalsDashboard</div>
-  )
+    return (
+        <div>GoalsDashboard
+            <div>Progress Chart overall</div>
+            <div>Set Goals input
+                <div>Goals in More details</div>
+          </div>
+          <div>Info
+            <div>How many goals this week</div>
+            <div>Count down days left</div>
+            <div>calendar</div>
+          </div>
+          <div>Progress pie this week
+            <div>Motivation text</div>
+          </div>
+        </div>
+
+    )
 }
 
 export default GoalsDashboard
