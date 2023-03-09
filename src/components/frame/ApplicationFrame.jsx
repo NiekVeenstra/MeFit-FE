a3f4dd83f457cbf185a7ef908e51b57abaf881ad
“Niek” <n.veenstra02@gmail.com>
2023-03-09 12:09:32 +0100

    add jwt decoder and fetch username

--- src/components/frame/ApplicationFrame.jsx
+++ src/components/frame/ApplicationFrame.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import DropdownMenu from "../navbar/DropdownMenu";
+import decode from "jwt-decode";
+import keycloak from "../../keycloak";
 
 const ApplicationFrame = () => {
+  const decodedToken = decode(keycloak.token);
+  const userName = decodedToken.name;
+
   return (
     <div>
       <h1>MeFit</h1>
       <div>
         <img src="" alt="" />
-        <h3>userName</h3>
+        <h3>{userName}</h3>
       </div>
       <DropdownMenu />
     </div>
