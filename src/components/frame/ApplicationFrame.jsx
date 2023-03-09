30bd3720a9702634a854f1f2b95090c93a052667
“Niek” <n.veenstra02@gmail.com>
2023-03-09 12:26:01 +0100

    add ternary operator and removes name if width<400

--- src/components/frame/ApplicationFrame.jsx
+++ src/components/frame/ApplicationFrame.jsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DropdownMenu from "../navbar/DropdownMenu";
 import decode from "jwt-decode";
 import keycloak from "../../keycloak";
+import styled from "styled-components";
+
+const StyledApplicationFrameContainer = styled.div`
+  background-color: ${(props) => props.theme.colors.backgroundColor};
+  color: ${(props) => props.theme.colors.textColorDark};
+  width: 100%;
+  height: 3.3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 3rem;
+  border-bottom: solid 0.15rem ${(props) => props.theme.colors.mainColor};
+`;
 
 const ApplicationFrame = () => {
   const decodedToken = decode(keycloak.token);
   const userName = decodedToken.name;
 
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div>
+    <StyledApplicationFrameContainer>
       <h1>MeFit</h1>
-      <div>
-        <img src="" alt="" />
-        <h3>{userName}</h3>
-      </div>
+      {screenWidth > 400 && <h3>{userName}</h3>}
+
       <DropdownMenu />
-    </div>
+    </StyledApplicationFrameContainer>
   );
 };
 
