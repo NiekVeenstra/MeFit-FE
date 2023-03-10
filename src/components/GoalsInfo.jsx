0af9f776c759ddbe8bf7b28707fe3d3d16389cd9
“Niek” <n.veenstra02@gmail.com>
2023-03-10 18:50:15 +0100

    fix: Style to style to prevent errors

--- src/components/GoalsInfo.jsx
+++ src/components/GoalsInfo.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import Calendar from './Calendar'
-import DaysLeft from './DaysLeft'
+import React from "react";
+import Calendar from "./Calendar";
+import DaysLeft from "./DaysLeft";
 
 const GoalsInfo = () => {
-    return (
-        <div Style="border: solid 3px black; width: 700px;">GoalsInfo
-            <div>How many goals this week</div>
-            <div Style="border: solid 3px black; width: 250px;">Days left to complete your goals <DaysLeft /></div>
-            <div><Calendar /></div>
-        </div>
-    )
-}
+  return (
+    <div style={{ border: "solid 3px black", width: "700px" }}>
+      GoalsInfo
+      <div>How many goals this week</div>
+      <div style={{ border: "solid 3px black", width: "250px" }}>
+        Days left to complete your goals <DaysLeft />
+      </div>
+      <div>
+        <Calendar />
+      </div>
+    </div>
+  );
+};
 
-export default GoalsInfo
+export default GoalsInfo;
