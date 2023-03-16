15e00129d5a122914b2a18b6daa20f3c4ac8157f
Meg <megstitu@gmail.com>
2023-03-16 17:16:48 +0100

    Nick fixed the api separation

--- src/components/dashboard/ProgressBar.jsx
+++ src/components/dashboard/ProgressBar.jsx
@@ -4,25 +4,28 @@ import { getWorkouts } from '../../api/apiCall/workouts';
 function ProgressBar() {
   const [completedPercentage, setCompletedPercentage] = useState(0);
 
-  useEffect(() => {
-    const data= getWorkouts() 
-    // Calculate the percentage of completed workouts
-    const completedWorkouts = data.filter(workouts => workouts.complete === true);
+  const fetchData = async () => {
+    const data = await getWorkouts();
+    console.log(data);
+    const completedWorkouts = data.filter((workouts) => workouts.complete === true);
     const percentage = (completedWorkouts.length / data.length) * 100;
     setCompletedPercentage(percentage);
-  });
-    return (
-      <div>
-        <p>Completed: {completedPercentage.toFixed(2)}%</p>
-        <div>
-          <progress value={completedPercentage} max="100" />
-        </div>
-        <div className="progress-bar">
-          <div className="progress-bar-fill" style={{ width: `${completedPercentage}%` }}></div>
-        </div>
-      </div>
-    );
   }
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <p>Completed: {completedPercentage.toFixed(2)}%</p>
+      <div>
+        <progress value={completedPercentage} max="100" />
+      </div>
+      <div className="progress-bar">
+        <div className="progress-bar-fill" style={{ width: `${completedPercentage}%` }}></div>
+      </div>
+    </div>
+  );
+}
 
 export default ProgressBar;
 
