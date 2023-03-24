f9b59775bc67124ab42afb919a121684c1bddf36
Meg <megstitu@gmail.com>
2023-03-24 17:21:03 +0100

    add styling

--- src/components/dashboard/Calendar.jsx
+++ src/components/dashboard/Calendar.jsx
@@ -1,17 +1,25 @@
 import * as React from "react";
 import dayjs from "dayjs";
+import styled from "styled-components";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
+const StyleProvider = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: calc(30vh - 3.3rem);
+  }
+`;
 export default function FirstComponent() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker style={{ width: "20rem", border: "solid blue 2px", borderRadius: "10px" ,  }} 
-        defaultValue={dayjs(Date.now())}
-      />
-    </LocalizationProvider>
+    <StyleProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker defaultValue={dayjs(Date.now())} />
+      </LocalizationProvider>
+    </StyleProvider>
   );
 }
-
-
