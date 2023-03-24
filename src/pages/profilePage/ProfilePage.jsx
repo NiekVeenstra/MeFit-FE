ce3545b384e4aac2bcee5361263b0e63cd5cf626
“Niek” <n.veenstra02@gmail.com>
2023-03-24 13:06:03 +0100

    fix: update reload img with svg

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -73,10 +73,14 @@ const StyledReloadButton = styled.button`
   position: absolute;
   align-self: end;
 
-  img{
+  img {
     width: 1.3rem;
   }
 `;
+
+const StyledSvg = styled.svg`
+  width: 1.3rem;
+`
 
 const ProfilePage = () => {
   const { user } = useUser({});
@@ -133,10 +137,9 @@ const ProfilePage = () => {
           </StyledImageContainer>
           <StyledProfileInfoContainer>
             <StyledReloadButton onClick={() => getUserData()}>
-              <img
-                src="https://cdn.icon-icons.com/icons2/1812/PNG/512/4213447-arrow-load-loading-refresh-reload-restart-sync_115423.png"
-                alt=""
-              />
+              <StyledSvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M447.5 224H456c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L397.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L311 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H447.5z" />
+              </StyledSvg>
             </StyledReloadButton>
             <div>
               Name: {user?.firstName} {user?.lastName}
